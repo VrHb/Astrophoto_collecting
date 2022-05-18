77a32c440777b995eb0ed4504e2affe7cc3effd1
Victor Abdukarimov <viredmi@gmail.com>
2022-05-18 12:01:55 +0300

    Publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -14,12 +14,14 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 async def main():
     bot = telegram.Bot(str(BOT_TOKEN))
     async with bot:
-        pprint.pprint(await bot.get_me())
-        pprint.pprint((await bot.get_updates())[7])
         await bot.send_message(
                 text="Hi There, a'm a Space Owl!", 
                 chat_id=-642797640
         )
+        await bot.send_photo(
+            chat_id=-642797640, 
+            photo=open("epic_images/epic_1b_20220516030610.png", "rb")
+            )
 
 if __name__ == "__main__":
     asyncio.run(main())
