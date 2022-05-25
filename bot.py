5431ec6e72b97ca5631cb403a77af8302ff941fd
Victor Abdukarimov <viredmi@gmail.com>
2022-05-25 12:13:03 +0300

    Fixed open function

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 import asyncio
 import telegram
+from telegram._utils.types import FileInput
 
 PHOTO_DIR = "images"
 
@@ -17,30 +18,35 @@ def get_filenames_list() -> list[str] | None:
         return list_filenames
    
 
-async def main(bot_token: str | None, chat_id: str | None):
+async def main(bot_token: str | None, chat_id: str | None, file: FileInput):
     bot = telegram.Bot(bot_token)
     async with bot:
         await bot.send_message(
-                text="Hi There, a'm a Space Owl!",
-                chat_id=chat_id
+            text="Hi There, a'm a Space Owl!",
+            chat_id=chat_id
         )
         await bot.send_photo(
             chat_id=chat_id,
-            photo=open(f"images/{file}", "rb")
+            photo=file
         )
 
 
 if __name__ == "__main__":
     load_dotenv()
-    
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     CHAT_ID = os.getenv("CHAT_ID")
     LATENCY = int(os.getenv("LATENCY"))
-    
     list_files = get_filenames_list()
-    
     while True:
         for file in list_files:
-            asyncio.run(main(bot_token=BOT_TOKEN, chat_id=CHAT_ID))
+            with open(f"images/{file}", "rb") as file:
+                file_input = file.read()
+            asyncio.run(
+                main(
+                    bot_token=BOT_TOKEN,
+                    chat_id=CHAT_ID,
+                    file=file_input
+                )
+            )
             time.sleep(LATENCY)
 
