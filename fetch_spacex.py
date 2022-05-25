340bbc16db7e3381fc8db46de82b8318152bd5d1
Victor Abdukarimov <viredmi@gmail.com>
2022-05-25 10:12:15 +0300

    Fixed code style

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -9,7 +9,8 @@ IMAGES_DIR = "images/"
 def fetch_spacex_launch(id_launch: str) -> None:
     """fetch sacex launch by id_launch"""
     response = requests.get(
-        url=f"https://api.spacexdata.com/v4/launches/{id_launch}")
+        url=f"https://api.spacexdata.com/v4/launches/{id_launch}"
+    )
     spacex_launches_list = response.json()
     spacex_image_links = spacex_launches_list["links"]["flickr"]["original"]
     for index, link in enumerate(spacex_image_links, start=1):
